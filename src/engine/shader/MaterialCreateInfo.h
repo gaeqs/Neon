046a9a918545ae689a05de9c53e81dc437b86641
Gael Rial Costas <gael.rial.costas@gmail.com>
2023-02-13 13:06:23 +0100

    Staring new material system.

---
+++ src/engine/shader/MaterialCreateInfo.h
@@ -0,0 +1,235 @@
+//
+// Created by gaelr on 11/02/2023.
+//
+
+#ifndef NEON_MATERIALCREATEINFO_H
+#define NEON_MATERIALCREATEINFO_H
+
+#include <memory>
+#include <vector>
+
+#include <engine/render/FrameBuffer.h>
+#include <engine/shader/ShaderProgram.h>
+#include <engine/shader/ShaderUniformDescriptor.h>
+#include <engine/structure/IdentifiableWrapper.h>
+#include <engine/model/InputDescription.h>
+
+enum class BlendingLogicOperation {
+    CLEAR,
+    AND,
+    AND_REVERSE,
+    COPY,
+    AND_INVERTED,
+    NO_OP,
+    XOR,
+    OR,
+    NOR,
+    EQUIVALENT,
+    INVERT,
+    OR_REVERSE,
+    COPY_INVERTED,
+    OR_INVERTED,
+    NAND,
+    SET,
+};
+
+enum class BlendOperation {
+    ADD = 0,
+    SUBTRACT = 1,
+    REVERSE_SUBTRACT = 2,
+    MIN = 3,
+    MAX = 4
+};
+
+enum ColorWriteMask {
+    R = 0x1,
+    G = 0x2,
+    B = 0x4,
+    A = 0x8
+};
+
+
+enum class BlendFactor {
+    ZERO = 0,
+    ONE = 1,
+    SRC_COLOR = 2,
+    ONE_MINUS_SRC_COLOR = 3,
+    DST_COLOR = 4,
+    ONE_MINUS_DST_COLOR = 5,
+    SRC_ALPHA = 6,
+    ONE_MINUS_SRC_ALPHA = 7,
+    DST_ALPHA = 8,
+    ONE_MINUS_DST_ALPHA = 9,
+    CONSTANT_COLOR = 10,
+    ONE_MINUS_CONSTANT_COLOR = 11,
+    CONSTANT_ALPHA = 12,
+    ONE_MINUS_CONSTANT_ALPHA = 13,
+    SRC_ALPHA_SATURATE = 14,
+    SRC1_COLOR = 15,
+    ONE_MINUS_SRC1_COLOR = 16,
+    SRC1_ALPHA = 17,
+    ONE_MINUS_SRC1_ALPHA = 18,
+};
+
+enum class DepthCompareOperation {
+    NEVER,
+    LESS,
+    EQUAL,
+    LESS_OR_EQUAL,
+    GREATER,
+    NOT_EQUAL,
+    GREATER_OR_EQUAL,
+    ALWAY,
+};
+
+struct MaterialDescriptions {
+
+    /**
+     * The description of the vertex buffer.
+     */
+    InputDescription vertex = InputDescription(0, InputRate::VERTEX);
+
+    /**
+     * The description of the instance buffer.
+     */
+    InputDescription instance = InputDescription(0, InputRate::INSTANCE);
+
+    /**
+     * The description of the material's uniform buffer.
+     */
+    std::shared_ptr<ShaderUniformDescriptor> uniform = nullptr;
+
+};
+
+struct MaterialAttachmentBlending {
+
+    /**
+     * The color channels the attachment can write to.
+     */
+    uint32_t writeMask = ColorWriteMask::R | ColorWriteMask::G |
+                         ColorWriteMask::B | ColorWriteMask::A;
+
+    /**
+     * Whether blending is enabled for the attachment.
+     */
+    bool blend = false;
+
+    /**
+     * The blending operation to perform on the color channel.
+     */
+    BlendOperation colorBlendOperation = BlendOperation::ADD;
+
+    /**
+     * The blend factor applied to the source color.
+     */
+    BlendFactor colorSourceBlendFactor = BlendFactor::SRC_ALPHA;
+
+    /**
+     * The blend factor applied to the destiny color.
+     */
+    BlendFactor colorDestinyBlendFactor = BlendFactor::ONE_MINUS_SRC_ALPHA;
+
+    /**
+     * The blending operation to perform on the alpha channel.
+     */
+    BlendOperation alphaBlendOperation = BlendOperation::ADD;
+
+    /**
+     * The blend factor applied to the source alpha.
+     */
+    BlendFactor alphaSourceBlendFactor = BlendFactor::ONE;
+
+    /**
+     * The blend factor applied to the destiny alpha.
+     */
+    BlendFactor alphaDestinyBlendFactor = BlendFactor::ZERO;
+
+};
+
+struct MaterialBlending {
+
+    /**
+     * Whether logic blending operations are enabled.
+     */
+    bool logicBlending = false;
+
+    /**
+     * The logic blending operator.
+     */
+    BlendingLogicOperation logicOperation = BlendingLogicOperation::CLEAR;
+
+    /**
+     * The blending settings for the material's attachments.
+     */
+    std::vector<MaterialAttachmentBlending> attachmentsBlending;
+
+    /**
+     * The blending constants to use in blending operations.
+     */
+    float blendingConstants[4] = {0};
+
+};
+
+struct MaterialDepthStencil {
+
+    /**
+     * Whether depth test is enabled for the material.
+     */
+    bool depthTest = true;
+
+    /**
+     * Whether depth write is enabled for the material.
+     */
+    bool depthWrite = true;
+
+    /**
+     * The compare operation for the depth test.
+     */
+    DepthCompareOperation depthCompareOperation = DepthCompareOperation::LESS;
+
+    /**
+     * Whether depth
+     */
+    bool useDepthBounds = false;
+
+    /**
+     * The minimum bounds of the depth test.
+     */
+    float minDepthBounds = 0.0f;
+
+    /**
+     * The maximum bounds of the depth test.
+     */
+    float maxDepthBounds = 1.0f;
+
+};
+
+struct MaterialCreateInfo {
+
+    /**
+     * The target framebuffer for the material.
+     * This is the framebuffer where models using
+     * the material will be drawn.
+     */
+    std::shared_ptr<FrameBuffer> target = nullptr;
+
+    /**
+     * The shader the material will use.
+     */
+    IdentifiableWrapper<ShaderProgram> shader = nullptr;
+
+    /**
+     * The buffer descriptions for vertex,
+     * instance and material uniform data.
+     */
+    MaterialDescriptions descriptions = MaterialDescriptions();
+
+    /**
+     * The blending settings for the
+     * material.
+     */
+    MaterialBlending blending = MaterialBlending();
+
+};
+
+#endif //NEON_MATERIALCREATEINFO_H
