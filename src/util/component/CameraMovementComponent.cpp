d577166148d17de518d57f67f10effb5da44453b
Gael Rial Costas <gael.rial.costas@gmail.com>
2024-07-31 02:50:57 +0200

    Improve CameraMovementComponent.

--- src/util/component/CameraMovementComponent.cpp
+++ src/util/component/CameraMovementComponent.cpp
@@ -10,16 +10,50 @@
 #include <engine/io/CursorEvent.h>
 
 namespace neon {
-    CameraMovementComponent::CameraMovementComponent() :
-            _w(),
-            _a(),
-            _s(),
-            _d(),
-            _shift(),
-            _space(),
-            _speed(1.0f),
-            _eulerAngles() {
+    void CameraMovementComponent::updateRotation(float deltaTime) {
+        if (_eulerAngles == _toEulerAngles) return;
 
+        _eulerAngles = (_eulerAngles + _toEulerAngles) / 2.0f;
+
+        auto qX = rush::Quatf::angleAxis(_eulerAngles.x(), {1.0f, 0.0f, 0.0f});
+        auto qY = rush::Quatf::angleAxis(_eulerAngles.y(), {0.0f, 1.0f, 0.0f});
+        getRoom()->getCamera().setRotation(qY * qX);
+    }
+
+    void CameraMovementComponent::updatePosition(float deltaTime) {
+        constexpr float EPSILON = 0.0001f;
+
+        auto& camera = getGameObject()->getRoom()->getCamera();
+        rush::Vec3f direction;
+
+        rush::Vec3f forwardClamped = camera.getForward();
+        forwardClamped.y() = 0.0f;
+        forwardClamped = forwardClamped.normalized();
+
+        if (_w) direction += forwardClamped;
+        if (_s) direction -= forwardClamped;
+        if (_d) direction += camera.getRight();
+        if (_a) direction -= camera.getRight();
+        if (_space) direction += rush::Vec3f(0.0f, 1.0f, 0.0f);
+        if (_shift) direction -= rush::Vec3f(0.0f, 1.0f, 0.0f);
+
+        if (std::abs(direction.x()) < EPSILON
+            && std::abs(direction.y()) < EPSILON
+            && std::abs(direction.z()) < EPSILON)
+            return;
+
+        auto offset = direction.normalized() * (_speed * deltaTime);
+        camera.move(offset);
+    }
+
+    CameraMovementComponent::CameraMovementComponent() : _w(),
+        _a(),
+        _s(),
+        _d(),
+        _shift(),
+        _space(),
+        _speed(1.0f),
+        _eulerAngles() {
     }
 
     void CameraMovementComponent::onKey(const KeyboardEvent& event) {
@@ -53,40 +87,13 @@ namespace neon {
     void CameraMovementComponent::onCursorMove(const CursorMoveEvent& event) {
         constexpr float LIMIT = 1.5707963f - 0.1f; // PI / 2 - 0.1
 
-        auto& camera = getRoom()->getCamera();
-
-        _eulerAngles -= event.delta.cast<float>()(1, 0).toVec() / 100.0f;
-        _eulerAngles.x() = std::clamp(_eulerAngles.x(), -LIMIT, LIMIT);
-
-        auto qX = rush::Quatf::angleAxis(_eulerAngles.x(), {1.0f, 0.0f, 0.0f});
-        auto qY = rush::Quatf::angleAxis(_eulerAngles.y(), {0.0f, 1.0f, 0.0f});
-        camera.setRotation(qY * qX);
+        _toEulerAngles -= event.delta.cast<float>()(1, 0).toVec() / 100.0f;
+        _toEulerAngles.x() = std::clamp(_toEulerAngles.x(), -LIMIT, LIMIT);
     }
 
     void CameraMovementComponent::onUpdate(float deltaTime) {
-        constexpr float EPSILON = 0.0001f;
-
-        auto& camera = getGameObject()->getRoom()->getCamera();
-        rush::Vec3f direction;
-
-        rush::Vec3f forwardClamped = camera.getForward();
-        forwardClamped.y() = 0.0f;
-        forwardClamped = forwardClamped.normalized();
-
-        if (_w) direction += forwardClamped;
-        if (_s) direction -= forwardClamped;
-        if (_d) direction += camera.getRight();
-        if (_a) direction -= camera.getRight();
-        if (_space) direction += rush::Vec3f(0.0f, 1.0f, 0.0f);
-        if (_shift) direction -= rush::Vec3f(0.0f, 1.0f, 0.0f);
-
-        if (std::abs(direction.x()) < EPSILON
-            && std::abs(direction.y()) < EPSILON
-            && std::abs(direction.z()) < EPSILON)
-            return;
-
-        auto offset = direction.normalized() * (_speed * deltaTime);
-        camera.move(offset);
+        updatePosition(deltaTime);
+        updateRotation(deltaTime);
     }
 
     float CameraMovementComponent::getSpeed() const {
--- src/util/component/CameraMovementComponent.h
+++ src/util/component/CameraMovementComponent.h
@@ -11,19 +11,22 @@
 
 namespace neon {
     class CameraMovementComponent : public Component {
-
         bool _w, _a, _s, _d, _shift, _space;
         float _speed;
 
         rush::Vec2f _eulerAngles;
+        rush::Vec2f _toEulerAngles;
+
+        void updateRotation(float deltaTime);
+
+        void updatePosition(float deltaTime);
 
     public:
-
         CameraMovementComponent();
 
-        void onKey(const KeyboardEvent &event) override;
+        void onKey(const KeyboardEvent& event) override;
 
-        void onCursorMove(const CursorMoveEvent &event) override;
+        void onCursorMove(const CursorMoveEvent& event) override;
 
         void onUpdate(float deltaTime) override;
 
@@ -31,6 +34,7 @@ namespace neon {
 
         void setSpeed(float speed);
     };
+
     REGISTER_COMPONENT(CameraMovementComponent, "Camera Move")
 }
 
