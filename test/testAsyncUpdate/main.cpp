2496cf4262a3267efa9c98f23de944612871b67a
Gael Rial <gael.rial.costas@gmail.com>
2024-09-25 15:37:56 +0200

    All tests working.

--- test/testAsyncUpdate/main.cpp
+++ test/testAsyncUpdate/main.cpp
@@ -679,8 +679,16 @@ std::shared_ptr<Room> getTestRoom(Application* application) {
 int main() {
     std::srand(std::time(nullptr));
 
-    Application application(std::make_unique<vulkan::VKApplication>(
-        "Neon", WIDTH, HEIGHT));
+    vulkan::VKApplicationCreateInfo info;
+    info.name = "Neon";
+    info.windowSize = {WIDTH, HEIGHT};
+
+    info.featuresConfigurator = [](const auto& d, auto& f) {
+        vulkan::VKApplicationCreateInfo::defaultFeaturesConfigurer(d, f);
+        f.basicFeatures.samplerAnisotropy = true;
+    };
+
+    Application application(std::make_unique<vulkan::VKApplication>(info));
 
     application.init();
     application.setRoom(getTestRoom(&application));
