cab978ae8020156c0297c44dac637af490137d06
Gael RIal Costas <gael.rial.costas@gmail.com>
2025-01-17 12:13:12 +0100

    Add documentation to Chronometer.

--- src/neon/util/Chronometer.h
+++ src/neon/util/Chronometer.h
@@ -7,20 +7,45 @@
 #include <chrono>
 
 namespace neon {
+    /**
+    * Small util class used to measure durations.
+    * <p>
+    * The start time is the time point the instance is created.
+    * You can reset the start time using the method 'reset()'.
+    */
     class Chronometer {
         std::chrono::high_resolution_clock::time_point _start;
 
     public:
+        /**
+        * Creates a chronometer.
+        * The count start at the creation of the instance.
+        */
         Chronometer();
 
+        /**
+        * Returns the time elapsed since the start time.
+        */
         [[nodiscard]] std::chrono::nanoseconds elapsed() const;
 
+        /**
+        * Returns the time elapsed since the start time in nanoseconds.
+        */
         [[nodiscard]] size_t elapsedNanoseconds() const;
 
+        /**
+        * Returns the time elapsed since the start time in milliseconds.
+        */
         [[nodiscard]] float elapsedMilliseconds() const;
 
+        /**
+        * Returns the time elapsed since the start time in seconds.
+        */
         [[nodiscard]] float elapsedSeconds() const;
 
+        /**
+        * Resets the start time.
+        */
         void reset();
     };
 }
