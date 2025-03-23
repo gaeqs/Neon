7c819a6f65af175ac42be549a46c7369dff90191
Gael Rial Costas <gael.rial.costas@urjc.es>
2025-03-23 03:17:31 +0100

    Update stb library.

--- lib/stb/src/dummy.cpp
+++
@@ -1,3 +0,0 @@
-//
-// Created by grial on 25/10/22.
-//
--- src/neon/render/texture/Texture.cpp
+++ src/neon/render/texture/Texture.cpp
@@ -8,7 +8,7 @@
 
 #define STB_IMAGE_IMPLEMENTATION
 
-#include <stb_image.h>
+#include <stb/stb_image.h>
 
 #include <neon/structure/Room.h>
 
